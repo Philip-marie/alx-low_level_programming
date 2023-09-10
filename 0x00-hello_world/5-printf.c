3f1d0bce80bd16878b7ad6a8f8f6b691545ec210
Philip-marie <philipmarie241@gmail.com>
2023-09-10 16:35:50 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints
+ * Return: Always print 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
