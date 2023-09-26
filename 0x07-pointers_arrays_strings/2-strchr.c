2c44a4a1b2e9bb23bd77bc8f3cbad60310e407f8
Philip-marie <philipmarie241@gmail.com>
2023-09-26 11:41:18 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strstr - Entry point
+ * _strchr - Entry point
  * @s: imput
  * @c: imput
  * Return: Always 0 (success)
  */
-char *_strstr(char *s, char c)
+char *_strchr(char *s, char c)
 {
 	int i = 0;
 
