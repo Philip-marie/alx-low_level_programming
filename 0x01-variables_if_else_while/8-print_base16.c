5687b8384fabee06f074825614ba17659fa370e6
Philip-marie <philipmarie241@gmail.com>
2023-10-30 14:37:39 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	int d;
+
+	c = 'a';
+	d = 'o';
+	while
+		(d < 10) {
+			putchar(d + 'o');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
