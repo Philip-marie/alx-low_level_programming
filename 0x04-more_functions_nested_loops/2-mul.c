12289a6685b5f19c3830dfb2afab97e464e6342d
Philip-marie <philipmarie241@gmail.com>
2023-11-07 14:17:17 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Function checking for a digit (0 through)
+ * @a: For variable
+ * @b: For variable
+ * Return: Always (0)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
